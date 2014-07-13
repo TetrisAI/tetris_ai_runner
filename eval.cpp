9385b3ae654cfc9eb70d453c32c88be3e5886403
ZouZhiZhang <mm304321141@gmail.com>
2014-07-13 12:50:02 +0800

    小改一下eval...提高性能

--- eval.cpp
+++ eval.cpp
@@ -13,7 +13,7 @@ int ai_eval(TetrisMap const &map, EvalParam *history, size_t history_length)
 {
     //行列变换
     int ColTrans = 2 * (map.height - map.roof);
-    int RowTrans = 0;
+    int RowTrans = map.roof == map.height ? 0 : map.width;
     for(int y = 0; y < map.roof; ++y)
     {
         if(!map.full(0, y))
@@ -24,91 +24,109 @@ int ai_eval(TetrisMap const &map, EvalParam *history, size_t history_length)
         {
             ++ColTrans;
         }
+        int TransBits = map.row[y] ^ (map.row[y] << 1);
         for(int x = 1; x < map.width; ++x)
         {
-            if(map.full(x - 1, y) ^ map.full(x, y))
+            if((TransBits >> x) & 1)
             {
                 ++ColTrans;
             }
         }
     }
-    for(int x = 0; x < map.width; ++x)
+    for(int y = 1; y < map.roof; ++y)
     {
-        if(!map.full(x, 0))
+        int TransBits = map.row[y - 1] ^ map.row[y];
+        for(int x = 0; x < map.width; ++x)
         {
-            ++RowTrans;
-        }
-        if(!map.full(x, map.height - 1))
-        {
-            ++RowTrans;
-        }
-        for(int y = 1; y < map.height; ++y)
-        {
-            if(map.full(x, y - 1) ^ map.full(x, y))
+            if((TransBits >> x) & 1)
             {
                 ++RowTrans;
             }
         }
     }
+    int Row0Bits = ~map.row[0];
+    int Row1Bits = map.roof == map.height ? ~map.row[map.roof - 1] : map.row[map.roof - 1];
+    for(int x = 0; x < map.width; ++x)
+    {
+        if((Row0Bits >> x) & 1)
+        {
+            ++RowTrans;
+        }
+        if((Row1Bits >> x) & 1)
+        {
+            ++RowTrans;
+        }
+    }
 
-    //洞数
-    int HoleCount = 0;
-    //洞行数
-    int HoleLine = 0;
-    //最高洞行数
-    int HolePosy = 0;
-    //最高洞上方块数
-    int HolePiece = 0;
+    struct
+    {
+        //洞数
+        int HoleCount;
+        //洞行数
+        int HoleLine;
+        //最高洞行数
+        int HolePosy;
+        //最高洞上方块数
+        int HolePiece;
 
-    //洞深,井深
-    int HoleDepth = 0;
-    int WellDepth = 0;
+        //洞深,井深
+        int HoleDepth;
+        int WellDepth;
 
-    //洞计数,井计数
-    int HoleNum[32] = {};
-    int WellNum[32] = {};
+        //洞计数,井计数
+        int HoleNum[32];
+        int WellNum[32];
 
-    int LineCover = 0;
-    int TopHole = 0;
+        //当前块行数
+        int LandHeight;
+        //设置左中右平衡破缺参数
+        int Middle;
+        //消行数
+        int EraseCount;
+
+        int LineCoverBits;
+        int TopHoleBits;
+    } v;
+    memset(&v, 0, sizeof v);
 
     for(int y = map.roof - 1; y >= 0; --y)
     {
-        LineCover |= map.row[y];
-        int LineHole = LineCover ^ map.row[y];
+        v.LineCoverBits |= map.row[y];
+        int LineHole = v.LineCoverBits ^ map.row[y];
         if(LineHole != 0)
         {
-            HoleLine++;
-            if(HolePosy == 0)
+            v.HoleLine++;
+            if(v.HolePosy == 0)
             {
-                HolePosy = y + 1;
-                TopHole = LineHole;
+                v.HolePosy = y + 1;
+                v.TopHoleBits = LineHole;
             }
         }
         for(int x = 0; x < map.width; ++x)
         {
             if((LineHole >> x) & 1)
             {
-                ++HoleCount;
-                ++HoleNum[x];
+                ++v.HoleCount;
+                ++v.HoleNum[x];
             }
             else
             {
-                HoleNum[x] = 0;
+                v.HoleNum[x] = 0;
             }
-            HoleDepth += HoleNum[x];
-            if(!((LineCover >> x) & 1) && (x == 0 || ((LineCover >> (x - 1)) & 1)) && (x == map.width - 1 || ((LineCover >> (x + 1)) & 1)))
+            v.HoleDepth += v.HoleNum[x];
+            if(!((v.LineCoverBits >> x) & 1) && (x == 0 || ((v.LineCoverBits >> (x - 1)) & 1)) && (x == map.width - 1 || ((v.LineCoverBits >> (x + 1)) & 1)))
             {
-                ++WellNum[x];
-                WellDepth += WellNum[x];
+                ++v.WellNum[x];
+                v.WellDepth += v.WellNum[x];
             }
         }
     }
-    if(HolePosy != 0)
+    if(v.HolePosy != 0)
     {
         //从最高有洞行上一行开始往上厉遍
-        for(int y = HolePosy; y < map.roof; ++y)
+        for(int y = v.HolePosy; y < map.roof; ++y)
         {
-            int CheckLine = TopHole & map.row[y];
+            int CheckLine = v.TopHoleBits & map.row[y];
             if(CheckLine == 0)
             {
                 break;
@@ -117,23 +135,17 @@ int ai_eval(TetrisMap const &map, EvalParam *history, size_t history_length)
             {
                 if((CheckLine >> x) & 1)
                 {
-                    HolePiece += y + 1;
+                    v.HolePiece += y + 1;
                 }
             }
         }
     }
-    //当前块行数
-    int LandHeight = 0;
-    //设置左中右平衡破缺参数
-    int Middle = 0;
-    //消行数
-    int EraseCount = 0;
     for(size_t i = 0; i < history_length; ++i)
     {
         TetrisNode const *node = history[i].node;
-        LandHeight += node->status.y + 1;
-        Middle += std::abs((node->status.x + 1) * 2 - map.width);
-        EraseCount += history[i].clear;
+        v.LandHeight += node->status.y + 1;
+        v.Middle += std::abs((node->status.x + 1) * 2 - map.width);
+        v.EraseCount += history[i].clear;
     }
 
     //死亡警戒
@@ -141,16 +153,16 @@ int ai_eval(TetrisMap const &map, EvalParam *history, size_t history_length)
 
     int hl = history_length;
     return int(0
-               - LandHeight * 200 / map.height / hl
-               + EraseCount * 6 / hl
+               - v.LandHeight * 200 / map.height / hl
+               + v.EraseCount * 6 / hl
                - ColTrans * 8
                - RowTrans * 8
-               - HoleCount * 6
-               - HoleLine * 38
-               - WellDepth * 10
-               - HoleDepth * 4
-               - HolePiece * 0.5
-               + Middle * 0.2 / hl
+               - v.HoleCount * 6
+               - v.HoleLine * 38
+               - v.WellDepth * 10
+               - v.HoleDepth * 4
+               - v.HolePiece * 0.5
+               + v.Middle * 0.2 / hl
                - BoardDeadZone * 5000
                );
 }
