d8f9a234ff5a9cb82c34f5f4ec352dfbf0163a34
奏之章 <mm304321141@gmail.com>
2014-07-28 16:23:41 +0800

    从java那挖到个BitCount函数...果断换上了-.-

--- src/ai_ax.cpp
+++ src/ai_ax.cpp
@@ -11,12 +11,13 @@ namespace
 {
     int BitCount(unsigned int n)
     {
-        n = (n & 0x55555555) + ((n >> 1) & 0x55555555);
+        // HD, Figure 5-2
+        n = n - ((n >> 1) & 0x55555555);
         n = (n & 0x33333333) + ((n >> 2) & 0x33333333);
-        n = (n & 0x0f0f0f0f) + ((n >> 4) & 0x0f0f0f0f);
-        n = (n & 0x00ff00ff) + ((n >> 8) & 0x00ff00ff);
-        n = (n & 0x0000ffff) + ((n >> 16) & 0x0000ffff);
-        return n;
+        n = (n + (n >> 4)) & 0x0f0f0f0f;
+        n = n + (n >> 8);
+        n = n + (n >> 16);
+        return n & 0x3f;
     }
 }
 
